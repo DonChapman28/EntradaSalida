c62e258c01dd5da6ca1521eeb168d42890794bd7
eSponjosa <e.rojascisterna@outlook.com>
2024-02-15 14:12:40 -0300

    avance 1
    camara y botones entrada/salida

--- appEntrada/src/app/entrada/entrada.page.ts
+++ appEntrada/src/app/entrada/entrada.page.ts
@@ -12,7 +12,8 @@ import { ServicioFechaHoraService } from '../fechaHora/servicio-fecha-hora.servi
   styleUrls: ['./entrada.page.scss'],
 })
 export class EntradaPage implements OnInit {
-
+  
+  alertButtons = ['Aceptar'];
   entrada : boolean = true;
   private codeReader: BrowserQRCodeReader;
   private selectedDevice: VideoInputDevice | null;
@@ -35,4 +36,59 @@ export class EntradaPage implements OnInit {
     console.log(this.entrada)
   }
 
+  async iniciarCamara() {
+    try {
+      const constraints = { video: { facingMode: 'environment' } };
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  
+      if (stream) {
+        // Almacenamos el stream en la propiedad mediaStream
+      this.mediaStream = stream;
+        const codeReader = new BrowserQRCodeReader();
+        const videoInputDevices: VideoInputDevice[] = await codeReader.getVideoInputDevices();
+  
+        if (videoInputDevices && videoInputDevices.length > 0) {
+          const selectedDevice: VideoInputDevice = videoInputDevices[0];
+  
+          codeReader.decodeFromInputVideoDevice(selectedDevice.deviceId).then((result: Result) => {
+            /* console.log(result.getText()); */
+            console.log('enviado');
+          });
+          
+          const video = document.getElementById('video') as HTMLVideoElement;
+          video.srcObject = stream;
+          video.play();
+          console.log('funciona');
+        } else {
+          console.error('No se encontraron dispositivos de video.');
+        }
+      } else {
+        console.error('No se pudo obtener acceso a la cámara.');
+      }
+    } catch (error) {
+      console.error('Error al iniciar la cámara:', error);
+    }
+  }
+
+  async mostrarAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Registro exitoso',
+      buttons: this.alertButtons
+    });
+
+    await alert.present();
+ 
+    
+  }
+  
+  async mostrarError() {
+    const alert = await this.alertController.create({
+      header: 'Seccion incorrecta',
+      buttons: this.alertButtons
+    });
+
+    await alert.present(); 
+    
+  }
 }

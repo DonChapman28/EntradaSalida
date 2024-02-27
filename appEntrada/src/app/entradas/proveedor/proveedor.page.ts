e509d8835657e96f37f3d0f8bf1212c1aa5a4849
eSponjosa <e.rojascisterna@outlook.com>
2024-02-27 16:54:52 -0300

    update

--- appEntrada/src/app/entradas/proveedor/proveedor.page.ts
+++ appEntrada/src/app/entradas/proveedor/proveedor.page.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { BrowserQRCodeReader,BrowserPDF417Reader, Result, VideoInputDevice } from '@zxing/library';
+import { Router } from '@angular/router';
+import { ServicioFechaHoraService } from 'src/app/fechaHora/servicio-fecha-hora.service';
+import { Storage } from '@ionic/storage-angular';
+import { StorageService } from 'src/app/servicioStorage/storage.service';
+import { entradaService } from 'src/app/entradaService/entrada-servicio.service';
+
 
 @Component({
   selector: 'app-proveedor',
@@ -7,9 +17,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProveedorPage implements OnInit {
 
-  constructor() { }
+  alertButtons = ['Aceptar'];
+  entrada : boolean = true;
+
+  personas:any = [];
+  codigo: any;
+  fechaEntrada: any;
+  fechaSalida: any;
+
+  constructor(private router: Router,
+    private activated: ActivatedRoute,
+    private alertController: AlertController,
+    private toastController: ToastController,
+    private fechaHora: ServicioFechaHoraService,
+    private storage: StorageService,
+    private entradaService: entradaService
+   ) {}
 
   ngOnInit() {
+    console.log(this.fechaHora.getFechaHora());
+    console.log(this.entrada);
+    this.fechaEntrada = this.fechaHora.getFechaHora();
+    this.storage.init
+    this.storage.getAllRegistro().then(x=> {this.personas = x; console.log(this.personas);
+    });
   }
 
+  entradaPersona(){
+    this.entradaService.entradaQr();
+  }
+
+  salidaPersona(){
+    this.entradaService.salidaQr();
+  }
+
+  async mostrarError() {
+    const alert = await this.alertController.create({
+      header: 'error',
+      buttons: this.alertButtons
+    });
+    await alert.present(); 
+    
+  }
+
+  
 }
+

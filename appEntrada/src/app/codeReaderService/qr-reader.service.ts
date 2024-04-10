3de27e04db8665982a4a1a4f480be7e97cc92eff
eSponjosa <e.rojascisterna@outlook.com>
2024-04-10 14:31:09 -0400

    cambio qr global

--- appEntrada/src/app/codeReaderService/qr-reader.service.ts
+++ appEntrada/src/app/codeReaderService/qr-reader.service.ts
@@ -51,7 +51,7 @@ const stream = await navigator.mediaDevices.getUserMedia({
     video: {
       width: { min: 640, ideal: 1920 },
       height: { min: 400, ideal: 1080 },
-      aspectRatio: { ideal: 1.7777777778 }
+      aspectRatio: { ideal: 0.5625 }
     }
   });;
 if (stream) {
